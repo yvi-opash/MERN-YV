a233d84434fe08a9df9e81f6f2e0912e743b89f5
Yash <yvi.opash@gmail.com>
2026-01-02 16:33:20 +0530

    two

---
+++ practice.js
@@ -0,0 +1,28 @@
+function multiply(num1 , num2){
+    console.log( num1 * num2);
+   return 0; 
+}
+
+multiply(15,2);
+
+function min_max(a){
+    let max = a[0];
+    let min = a[0];
+    for(let i=0; i<=a.length; i++){
+        if (a[i] >= max){
+            max = a[i];
+        }
+
+        if (a[i] <= min){
+            min = a[i];
+        }
+        
+    }
+    console.log(max, "is a maximum number");
+    console.log(min, "is a minimum number");
+    
+}
+
+const arr = [6, 8, 2, 5, 9, 45, 548 ,84 ,51 ,5, ] 
+
+min_max(arr);
